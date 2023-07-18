32d25a252350a631ce2bf17d517ebef8ebd181e7
doitkim <kimsh27214@gmail.com>
2023-07-18 22:02:53 +0900

    use hook with react typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback } from "react";
+import { useReducer, useState } from "react";
+import Greeting from "./components/GreetingFunctional";
+
+const reducer = (state: any, action: any) => {
+  console.log("enteredNameReducer");
+  switch (action.type) {
+    case "enteredName":
+      if (state.enteredName === action.payload) {
+        return state;
+      }
+      return {
+        ...state,
+        enteredName: action.payload,
+      };
+    case "message":
+      return {
+        ...state,
+        message: `Hello, ${action.payload}`,
+      };
+    default:
+      throw new Error("Invalid action type" + action.type);
+  }
+};
+
+const initialState = {
+  enteredName: "",
+  message: "",
+};
 
 function App() {
+  const [{ message, enteredName }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const [startCount, setStartCount] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const setCountCallback = useCallback(() => {
+    const inc =
+      count + 1 > startCount ? count + 1 : Number(count + 1) + startCount;
+    setCount(inc);
+  }, [count, startCount]);
+
+  const onWelcomeBtnClick = () => {
+    setCountCallback();
+  };
+
+  const onChangeStartCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStartCount(Number(e.target.value));
+  };
+
+  console.log("App.tsx render");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Greeting
+          message={message}
+          enteredName={enteredName}
+          greetingDispatcher={dispatch}
+        />
+
+        <div style={{ marginTop: "10px" }}>
+          <label>Enter a number and we'll inclement it</label>
+          <br />
+          <input
+            value={startCount}
+            onChange={onChangeStartCount}
+            style={{ width: ".75rem" }}
+          />
+          &nbsp;
+          <label>{count}</label>
+          <br />
+          <button onClick={onWelcomeBtnClick}>Increment count</button>
+        </div>
       </header>
     </div>
   );
